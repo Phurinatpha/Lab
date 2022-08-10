07e0797180d6f690b3d547a38bde4df546d1cbf4
Phurinatpha <phurinat_phaphong@cmu.ac.th>
2022-08-10 19:50:57 +0700

    x

--- Labpy/Lab7/lab7_2.py
+++ Labpy/Lab7/lab7_2.py
@@ -1,23 +1,20 @@
 
-def patterned_message(message, pattern):
+def patterned_message(message, pattern,i=0,j=0):
     message = message.replace(" ","")
-    printpat(message,pattern)
-
-def printpat(message, pattern,i=0,j=0):
     if i == len(pattern)-1:
         return ""
     elif pattern[i] == "*":
         print(message[j],end="")
         if j == len(message)-1:
-            return printpat(message,pattern,i+1,0)        
+            return patterned_message(message,pattern,i+1,0)        
         else:
-            return printpat(message,pattern,i+1,j+1)
+            return patterned_message(message,pattern,i+1,j+1)
     elif pattern[i] == "\n":
         print("\n",end="")
-        return printpat(message,pattern,i+1,j)
+        return patterned_message(message,pattern,i+1,j)
     elif pattern[i] == " ":
         print(" ",end="")
-        return printpat(message,pattern,i+1,j)
+        return patterned_message(message,pattern,i+1,j)
 
 def main():
     patterned_message("Three Diamonds!",
