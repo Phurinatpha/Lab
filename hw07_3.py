ccfc432e8ad13bf232437a6ff15a2eccd03e162f
Phurinat Phanuphongsakun <phurinat_phanuphong@cmu.ac.th>
2022-08-11 15:38:54 +0700

    a

---
+++ hw07_3.py
@@ -0,0 +1,13 @@
+def longest_digit_run(n,m=1,c=1):
+    n = str(n)
+    if len(n) == 0:
+        return m
+    elif n[0] == n[1:2]:
+        c +=1
+        m = max(m,c)
+        return longest_digit_run(n[1:],m,c)
+    else:
+        return longest_digit_run(n[1:],m,c=1)
+
+print(longest_digit_run(11777332))
+        
