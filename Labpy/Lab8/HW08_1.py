91893baa87b34cc81b0b9772acff245fc12af8b2
Phurinatpha <phurinat_phaphong@cmu.ac.th>
2022-08-16 01:50:14 +0700

    a

--- Labpy/Lab8/HW08_1.py
+++ Labpy/Lab8/HW08_1.py
@@ -1,12 +1,13 @@
 def main():
-    print(moving_average([1, 2, 3, 4, 5],3))
+    print(moving_average([1, 2, 3, 4, 5],2))
 
 def moving_average(list_a, w,ltout=[]):
-    if w-1==len(list_a):
-        return ltout
+    if w==len(list_a):
+        return [(sum(list_a[0:w])/w)]
+    elif w > len(list_a):
+        return []
     else :
-        ltout += [(sum(list_a[0:w])/w)]
-        return moving_average(list_a[1:],w,ltout)
+        return [(sum(list_a[0:w])/w)] + moving_average(list_a[1:],w,ltout)
 
 if __name__ == '__main__':
     main()
---
+++ Labpy/Lab8/HW08_2.py
@@ -0,0 +1,8 @@
+def main():
+    print(moving_average([1, 2, 3, 4, 5],2))
+
+def moving_average(list_a, w,ltout=[]):
+    
+
+if __name__ == '__main__':
+    main()
