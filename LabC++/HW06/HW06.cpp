4e9b74234382777e5ef7c169078f2891d5cb8610
Phurinatpha <phurinat_phaphong@cmu.ac.th>
2022-10-17 07:41:46 +0700

    17/10/65

--- LabC++/HW06/HW06.cpp
+++ LabC++/HW06/HW06.cpp
@@ -24,17 +24,65 @@ public:
         for(int i=0;i<n;i++){
             Team x(i,0,0);
             T.push_back(x);
+        
         }
     }
 
     void match(int ID1, int ID2,int G1,int G2){
-        /*
-         WRITE YOUR CODE HERE
-         */
+        int posid1,posid2;
+
+        for (int i = 0; i < numberOfTeams; i++)
+        {   
+            if (T[i].ID == ID1){
+                posid1 = i;
+            }
+            else{
+                posid2 = i;
+            }
+        }
+
+        if (G1 > G2) {
+            T[posid1].PTS += 3;
+            T[posid1].GD += G1 - G2;
+            T[posid2].GD -= G1 - G2;
+        } else if (G2 > G1){
+            T[posid2].PTS += 3;
+            T[posid2].GD += G2 - G1;
+            T[posid1].GD -= G2 - G1;
+        } else {
+            T[posid1].PTS += 1;
+            T[posid2].PTS += 1;
+        }
+        sorted();
     } 
+
     void showTeamAtRank(int i){
-        /*
-         WRITE YOUR CODE HERE
-         */
+        
+        cout << T[i-1].ID << " ";
+        cout << T[i-1].PTS << " ";
+        cout << T[i-1].GD << endl;
+    }
+    
+    void sorted(){
+        for (int i = numberOfTeams-1; i >= 0 ; i--){
+            for (int j = 1; j <= i; j++){
+                if (T[j-1].PTS < T[j].PTS){
+                    Team temp(T[j-1].ID,T[j-1].GD,T[j-1].PTS);
+                    T[j-1] = T[j];
+                    T[j] = temp;
+                }else if(T[j-1].PTS == T[j].PTS){
+                    if (T[j-1].GD < T[j].GD){
+                        Team temp(T[j-1].ID,T[j-1].GD,T[j-1].PTS);
+                        T[j-1] = T[j];
+                        T[j] = temp;
+                    }
+                    else if( T[j-1].GD == T[j].GD && (T[j-1].ID > T[j].ID)){
+                        Team temp(T[j-1].ID,T[j-1].GD,T[j-1].PTS);
+                        T[j-1] = T[j];
+                        T[j] = temp;
+                    }     
+                }
+            }  
+        }
     }
 };
--- LabC++/HW06/main.cpp
+++ LabC++/HW06/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
     int n;
-    cin>>n;
-    Scoreboard S(n);
+    // cin>>n;
+    Scoreboard S(4);
     S.showTeamAtRank(1) ;
     S.match(0,1,2,3);
     S.showTeamAtRank(1);
