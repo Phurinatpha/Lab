8f2983ea51aa61e2ee75c375eac20b7d37058218
Phurinatpha <phurinat_phaphong@cmu.ac.th>
2022-10-26 19:32:34 +0700

    26/10/65

--- LabC++/HW08/HW08.cpp
+++ LabC++/HW08/HW08.cpp
@@ -1,5 +1,5 @@
 #include "BSTNode.cpp"
-
+#include <iostream>
 class BST {
 
 public:
@@ -10,23 +10,96 @@ public:
   }
 
   void insert(int value) {
-    /*
-     Write your code here
-     */
+    BSTNode *p, *previous, *new_node = new BSTNode;
+    new_node->value = value;
+    new_node->left = nullptr;
+    new_node->right = nullptr;
+
+    if (root == nullptr){
+        root = new_node;
+    }else{
+      p = root;
+      while (p != nullptr){
+        previous = p;
+        if (p->value > new_node->value){
+          p = p->left;
+        }else {
+          p = p->right;
+        }
+      }
+
+      if (previous->value > new_node->value){
+        previous->left = new_node;
+      } else{
+        previous->right = new_node;
+      }
+    }
   }
 
 
   void remove(int value) {
-    /*
-     Write your code here
-     */
+    removePri(value,root);
   }
 
   int get_depth(int value) {
-    /*
-     Write your code here
-     */
+    int depth = 0;
+    BSTNode *p;
+
+    if (root == NULL){
+        return 0;
+    }
+
+    p = root;
+    while (p != nullptr){
+      if (p->value != value){
+        if (p->value > value){
+            p = p->left;}
+        else{
+            p = p->right;
+        }
+        depth++;
+      }else {
+        break;
+      }
+    }
+    if (p != nullptr){
+      return depth;
+    }
+
     return -1;
   }
 
+  void removePri(int value,BSTNode *&root){
+    if(root == NULL){
+      return;
+    }else if (value < root -> value){
+      removePri(value,root->left);
+    }else if (value > root -> value){
+      removePri(value,root->right);
+    }else{
+      if (root->left == NULL && root->right == NULL){
+        delete root;
+        root = NULL;
+      }else if (root->left == NULL){
+        BSTNode *temp = root;
+        root = root -> right;
+        delete temp;
+      }else if(root->right == NULL){
+        BSTNode *temp = root;
+        root = root -> left;
+        delete temp;
+      }else {
+        BSTNode *temp = findmin(root->right);
+        root -> value = temp -> value;
+        removePri(temp->value,root->right);
+      }
+    }
+  }
+
+  BSTNode* findmin(BSTNode * root){
+    while(root->left != NULL)
+      root = root -> left;
+    return root;
+  }
+    
 };
--- LabC++/HW08/main.cpp
+++ LabC++/HW08/main.cpp
@@ -8,10 +8,25 @@ int main() {
     tree.insert(2);
     tree.insert(1);
     tree.insert(3);
-    tree.remove(2);
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(4);
+    
     cout << tree.get_depth(1) << endl;
     cout << tree.get_depth(2) << endl;
     cout << tree.get_depth(3) << endl;
-
+    cout << tree.get_depth(4) << endl;
+    cout << tree.get_depth(5) << endl;
+    cout << tree.get_depth(6) << endl;
+    
+    tree.remove(2);
+    
+    cout << tree.get_depth(1) << endl;
+    cout << tree.get_depth(2) << endl;
+    cout << tree.get_depth(3) << endl;
+    cout << tree.get_depth(4) << endl;
+    cout << tree.get_depth(5) << endl;
+    cout << tree.get_depth(6) << endl;
+    
     return 0;
 }
