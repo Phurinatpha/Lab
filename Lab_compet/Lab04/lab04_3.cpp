9eb7d6a521c46c6dfbb6af130d274f2e4ad28d8b
Phurinatpha <phurinat_phanuphong@cmu.ac.th>
2022-12-01 19:00:50 +0700

    a

---
+++ Lab_compet/Lab04/lab04_3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    
+    
+    
+    
+    return 0;
+}
---
+++ Lab_compet/Lab04/lab04_4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long x;
+    cin >> x;
+    cout << (long long)ceil(sqrt(x)-1) ;
+    return 0;
+}
