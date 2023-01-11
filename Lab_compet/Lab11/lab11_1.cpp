00e0baca6c0fa69527d8a8524da1a3b638af3057
Phurinatpha <phurinat_phanuphong@cmu.ac.th>
2023-01-12 03:11:57 +0700

    a

--- Lab_compet/Lab11/lab11_1.cpp
+++ Lab_compet/Lab11/lab11_1.cpp
@@ -1,18 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
+    int n, m, num, be;
     cin >> n;
-    while (n--){
-        cin >> x;
-        min = abs(sum[0])
+    int arr_n[n];
+    vector<int> sum;
+    
+    for (int i = 0; i < n; i++){
+        cin >> arr_n[i];
     }
 
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            sum.push_back(arr_n[i] + arr_n[j]);
+        }
+    }
+
+    cin >> m;
+    while (m--){
+        cin >> num;
+        int min_m = 0;
+        min_m = abs(sum.front() - num);
+        for (auto a:sum){ 
+            if ( min_m > abs(a - num)){
+                min_m = abs(a - num);
+            } 
+        }
+        cout << min_m << endl;        
+    }
+    
 
     return 0;
-
-
 }
